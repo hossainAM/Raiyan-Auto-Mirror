3932b585cfc0907137e2c7de51151788a208b093
Amir Hossain <amir508_du@yahoo.com>
2022-05-24 07:26:00 +0600

    my order page created and info loaded

--- src/Pages/Dashboard/MyOrder.js
+++ src/Pages/Dashboard/MyOrder.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyOrder = () => {
+    const [orders, setOrders] = useState([]);
+    const [user] = useAuthState(auth);
+
+    useEffect(() => {
+        if(user) {
+            fetch(`http://localhost:5000/order?email=${user.email}`, {
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setOrders(data);
+            })
+        }
+    }, [user]);
+
     return (
         <div>
-            <h2>My Order</h2>
+            <h2 className='text-xl text-secondary font-bold my-4'>My Orders</h2>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Product</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                        <th>Payment</th>
+                        <th>Cancel Order</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            orders.map((order, index) => <tr key={index}>
+                                <th>{index  + 1}</th>
+                                <td>{order.name}</td>
+                                <td>{order.email}</td>
+                                <td>{order.product}</td>
+                                <td>{order.quantity}</td>
+                                <td>{order.price}</td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
